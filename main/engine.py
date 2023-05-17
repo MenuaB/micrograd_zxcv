9c0de9f8b09ce7006022ed0d36c1aedd70463018
menooa.vold@gmail.com <menooa.vold@gmail.com>
2023-05-17 17:12:08 +0400

    Added docstrings using chatgpt

--- main/engine.py
+++ main/engine.py
@@ -2,6 +2,16 @@ import math
 
 class Node:
     def __init__(self, value, _children=set(), _op=[], grad=0, _label="NL"):
+        """
+        Initialize a Node object.
+        
+        Args:
+        - value: The value of the node.
+        - _children: A set of child nodes.
+        - _op: A list of operations performed on the node.
+        - grad: The gradient of the node.
+        - _label: The label of the node.
+        """
         self.value = value
         self.grad = 0
         self._backward= lambda : None
@@ -10,9 +20,21 @@ class Node:
         self._op = _op
 
     def __repr__(self) -> str:
+        """
+        Return a string representation of the Node object.
+        """
         return f"Node(value = {self.value})"
     
     def __add__(self, other):
+        """
+        Add two Node objects or a Node object and a scalar value.
+        
+        Args:
+        - other: The other Node object or scalar value to be added.
+        
+        Returns:
+        - An output Node object representing the sum of the operands.
+        """
         other = other if isinstance(other, Node) else Node(other)
         out = Node(self.value + other.value, (self, other), '+')
         def _backward():
@@ -22,6 +44,15 @@ class Node:
         return out
     
     def __mul__(self, other):
+        """
+        Multiply two Node objects or a Node object and a scalar value.
+        
+        Args:
+        - other: The other Node object or scalar value to be multiplied.
+        
+        Returns:
+        - An output Node object representing the product of the operands.
+        """
         other = other if isinstance(other, Node) else Node(other)
         out = Node(self.value * other.value, (self, other), '*')
         def _backward():
@@ -31,6 +62,15 @@ class Node:
         return out
     
     def __pow__(self, k):
+        """
+        Raise the Node object to a power.
+        
+        Args:
+        - k: The power to which the Node object is raised.
+        
+        Returns:
+        - An output Node object representing the power of the operand.
+        """
         out = Node(self.value ** k, (self, ), f'**{k}')
         def _backward():
             self.grad += out.grad * k * self.value ** (k-1)
@@ -38,11 +78,26 @@ class Node:
         return out
     
     def __truediv__(self, other):
+        """
+        Divide two Node objects or a Node object and a scalar value.
+        
+        Args:
+        - other: The other Node object or scalar value to be divided by.
+        
+        Returns:
+        - An output Node object representing the division of the operands.
+        """
         other = other if isinstance(other, Node) else Node(other)
         out = self * other ** -1
         return out 
     
     def exp(self):
+        """
+        Compute the exponential value of the Node object.
+        
+        Returns:
+        - An output Node object representing the exponential value.
+        """
         out = Node(math.exp(self.value), (self, ), 'exp')
         def _backward():
             self.grad += out.grad * out.value
@@ -50,6 +105,12 @@ class Node:
         return out
     
     def tanh(self):
+        """
+        Compute the hyperbolic tangent value of the Node object.
+        
+        Returns:
+        - An output Node object representing the hyperbolic tangent value.
+        """
         x = self.value
         out = Node((math.exp(x*2)-1)/(math.exp(x*2)+1), (self, ), 'tanh')
         def _backward():
@@ -58,19 +119,61 @@ class Node:
         return out
     
     def __sub__(self, other):
+        """
+        Subtract two Node objects or a Node object and a scalar value.
+        
+        Args:
+        - other: The other Node object or scalar value to be subtracted.
+        
+        Returns:
+        - An output Node object representing the difference of the operands.
+        """
         return self + -1.0 * other
     
     def __radd__(self, other):
+        """
+        Add a Node object and a scalar value.
+        
+        Args:
+        - other: The scalar value to be added.
+        
+        Returns:
+        - An output Node object representing the sum of the operands.
+        """
         return self + other
     
     def __rmul__(self, other):
+        """
+        Multiply a Node object and a scalar value.
+        
+        Args:
+        - other: The scalar value to be multiplied.
+        
+        Returns:
+        - An output Node object representing the product of the operands.
+        """
         return self * other
     
     def __neg__(self):
+        """
+        Negate the Node object.
+        
+        Returns:
+        - An output Node object representing the negation of the operand.
+        """
         return self * -1 
     
     @staticmethod
     def get_topo(node):
+        """
+        Perform a topological sort on the graph of nodes.
+        
+        Args:
+        - node: The starting node for the topological sort.
+        
+        Returns:
+        - A list of nodes in the topological order.
+        """
         topo = []
         visited = set()
         def build_topo(v):
@@ -83,6 +186,9 @@ class Node:
         return build_topo(node)
         
     def backward(self):
+        """
+        Perform backpropagation to compute gradients of the nodes.
+        """
         topo_sort = self.get_topo(self)
         self.grad = 1.0
         for node in topo_sort[::-1]:
--- main/micrograd.py
+++ main/micrograd.py
@@ -3,46 +3,92 @@ from .engine import Node
 
 class Module:
     def parameters(self):
+        """Returns an empty list of parameters."""
         return []
     
     def zero_grad(self):
+        """Sets the gradients of all parameters to zero."""
         for p in self.parameters():
             p.grad = 0
 
 class Neuron(Module):
     def __init__(self, nin):
-        self.w = [Node(random.uniform(-1,1), _label=f'W{i}') for i in range(nin)]
-        self.b = Node(random.uniform(-1,1))
-        # print('w',len(self.w))
+        """Initializes a Neuron object with 'nin' input nodes."""
+        self.w = [Node(random.uniform(-1, 1), _label=f'W{i}') for i in range(nin)]
+        self.b = Node(random.uniform(-1, 1))
 
     def __call__(self, x):
-        act = sum((xi*wi for wi,xi in zip(self.w, x)), self.b) 
+        """
+        Computes the output of the neuron given an input 'x'.
+
+        Args:
+            x: Input to the neuron.
+
+        Returns:
+            The output of the neuron after applying the activation function.
+        """
+        act = sum((xi * wi for wi, xi in zip(self.w, x)), self.b)
         out = act.tanh()
         return out 
     
     def parameters(self):
-        return self.w +[self.b]
+        """Returns the list of parameters (weights and bias) of the neuron."""
+        return self.w + [self.b]
     
 class Layer(Module):
     def __init__(self, nin, nout):
+        """
+        Initializes a Layer object with 'nin' input nodes and 'nout' output nodes.
+
+        Args:
+            nin: Number of input nodes.
+            nout: Number of output nodes.
+        """
         self.nodes = [Neuron(nin) for _ in range(nout)]
 
     def __call__(self, x):
+        """
+        Computes the outputs of the layer given an input 'x'.
+
+        Args:
+            x: Input to the layer.
+
+        Returns:
+            The outputs of the layer as a list.
+        """
         outs = [n(x) for n in self.nodes]
         return outs[0] if len(outs) == 1 else outs
     
     def parameters(self):
+        """Returns the list of parameters (weights and biases) of all nodes in the layer."""
         return [p for node in self.nodes for p in node.parameters()]
     
 class MLP(Module):
     def __init__(self, nin, nouts):
+        """
+        Initializes a Multi-Layer Perceptron (MLP) object.
+
+        Args:
+            nin: Number of input nodes.
+            nouts: List containing sthe number of nodes in each layer of the MLP.
+        """
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
 
     def __call__(self, x):
+        """
+        Computes the output of the MLP given an input 'x'.
+
+        Args:
+            x: Input to the MLP.
+
+        Returns:
+            The output of the MLP.
+        """
         for l in self.layers:
             x = l(x)
         return x
     
     def parameters(self):
+        """Returns the list of parameters (weights and biases) of all layers in the MLP."""
         return [p for layer in self.layers for p in layer.parameters()]
